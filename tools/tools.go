ce52398f878dfe01e9ea887b891ca6e5d0bf052f
Alexandr Rutkowski <90549065+kitanoyoru@users.noreply.github.com>
2024-11-21 14:56:44 +0300

    feat: init project (#1)

---
+++ tools/tools.go
@@ -0,0 +1,11 @@
+//go:build tools
+
+package tools
+
+import (
+	_ "github.com/golang/mock/mockgen"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "golang.org/x/tools/cmd/goimports"
+	_ "gotest.tools/gotestsum"
+)
+
